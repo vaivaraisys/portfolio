5c7c052f4af6a3c44da9ecf6eeebaa89dd0feabf
vaivaraisys <143621592+vaivaraisys@users.noreply.github.com>
2023-11-08 11:18:09 -0500

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+// back to top button
+let myButton = document.querySelector("#topButton");
+
+
+function topFunction() {
+    console.log('top!')
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// welcome page on load
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('welcome-modal');
+    const closeButton = document.getElementById('close-welcome');
+  
+    modal.style.display = 'block';
+  
+    closeButton.addEventListener('click', function () {
+      modal.style.display = 'none';
+    });
+  });
